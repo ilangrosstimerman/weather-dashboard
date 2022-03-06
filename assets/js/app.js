a0ee82243cb9583d05ec93261e4c70c57a5fd3bb
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-06 15:25:49 -0800

    add search button and define getWeather function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+//src and variables
+
 src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 var fiveDayURL;
 var currentQueryURL;
@@ -6,3 +8,36 @@ var latitude;
 var longitude;
 var citySearch;
 var cityStorage;
+
+//local storage
+
+if (JSON.parse(localStorage.getItem("cityArray") !== null)) {
+    cityStorage = JSON.parse(localStorage.getItem("cityArray"));
+  } else {
+    cityStorage=[]
+    localStorage.setItem("cityArray", cityStorage);
+  }
+  
+//search button functionality
+
+  $("#searchSubBtn").on("click", function (event) {
+    $("#cityCurrent").html("");
+    $("#fiveDay").html("");
+    event.preventDefault();
+    citySearch = $("#searchBar").val().trim();
+    console.log(citySearch);
+    getWeather();
+  });
+
+  //define getWeather function
+  
+  function getWeather() {
+    var currentQueryURL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      citySearch +
+      "&appid=18a8feef5d0c615b7e0d94298dc9dfbe";
+
+    var fiveDayURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    citySearch +
+    "&appid=18a8feef5d0c615b7e0d94298dc9dfbe";
