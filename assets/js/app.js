411061bf0ea7828cd35c484385dcaac7868714a3
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-06 13:45:23 -0800

    add initial js variables

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+var fiveDayURL;
+var currentQueryURL;
+var UVIndURL;
+var latitude;
+var longitude;
+var citySearch;
+var cityStorage;
