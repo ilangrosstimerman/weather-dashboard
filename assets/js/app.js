061c1b4683bb26374e06285b2cec49ba0ef5a23c
ilantimerman <ilan.tmrmn@gmail.com>
2022-03-06 16:08:30 -0800

    adding api/ajax to complete initial js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,8 +42,6 @@ if (JSON.parse(localStorage.getItem("cityArray") !== null)) {
     citySearch +
     "&appid=18a8feef5d0c615b7e0d94298dc9dfbe";
 
-  }
-
 
   //today's info
 
@@ -71,4 +69,85 @@ if (JSON.parse(localStorage.getItem("cityArray") !== null)) {
     cityName = response.name;
     cityStorage.push(response.name);
   
-  })
+//uv index GET
+
+  $.ajax({
+    url: UVIndURL,
+    method: "GET",
+  }).then(function (responseUV) {
+    console.log(responseUV);
+    UVInd = responseUV.value;
+    var thisDate = response.dt;
+    var dateConvert = new Date(thisDate * 1000).toLocaleDateString("en-US");
+    console.log(dateConvert);
+    var currentWeather = $("<div>").attr("id", "currentWeather");
+    currentWeather.append(
+      "<h2>" +
+        cityName +
+        " (" +
+        dateConvert +
+        ") <img id='wicon' src='http://openweathermap.org/img/w/" +
+        response.weather[0].icon +
+        ".png' alt = 'weather icon'></h2>"
+    );
+    currentWeather.append(
+      "<div>Temperature: " + todayTemp.toFixed(1) + "&#176" + "F </h5>"
+    );
+    currentWeather.append("<div>Humidity: " + todayHumidity + "%</h5>");
+    currentWeather.append("<div>Wind Speed: " + todayWS + " MPH");
+    var currentLastLine = $("<div>").text("UV Index: ");
+    var UVIndexNum = $("<span>").text(UVInd).attr("id", "UVIndex");
+    if (UVInd < 3) {
+      UVIndexNum.attr("style", "background-color: green");
+    } else if (UVInd > 7) {
+      UVIndexNum.attr("style", "background-color: red");
+    } else {
+      UVIndexNum.attr("style", "background-color: yellow");
+    }
+    currentLastLine.append(UVIndexNum);
+    currentWeather.append(currentLastLine);
+    $("#cityCurrent").append(currentWeather);
+    $("#weatherInfo").attr("style", "visibility: visible");
+  });
+});
+
+//five day forecast GET
+
+$.ajax({
+    url: fiveDayURL,
+    method: "GET",
+  }).then(function (responseForecast) {
+    console.log(responseForecast);
+    for (i = 0; i < 5; i++) {
+      console.log(i * 8 + 4);
+      var date = responseForecast.list[i * 8 + 4].dt_txt;
+      var dateSplit = date.split(" ", 1)[0];
+      var dateSplit2 = dateSplit.split("-");
+      console.log(dateSplit2);
+
+      var finalDate = dateSplit2[1] + "/" + dateSplit2[2] + "/" + dateSplit2[0];
+      var newCard = $("<div>").attr("class", "card");
+      newCard.append("<div class='card-body'>");
+      newCard.append(
+        "<h5 class='card-title' id = 'cardHead'>" + finalDate + "</h5>"
+      );
+      var imgDiv = $(
+        "<div id = 'icon'><img id='wicon' src='http://openweathermap.org/img/w/" +
+          responseForecast.list[i * 8 + 4].weather[0].icon +
+          ".png' alt = 'weather icon'></div>"
+      );
+      newCard.append(imgDiv);
+      var newCardTemp =
+        (responseForecast.list[i * 8 + 4].main.temp - 273) * (9 / 5) + 32;
+      newCard.append(
+        "<div>Temp: " + newCardTemp.toFixed(1) + " &#176" + "F</div>"
+      );
+      newCard.append(
+        "<div>Humidity: " +
+          responseForecast.list[i * 8 + 4].main.humidity +
+          "%</h5>"
+      );
+      $("#fiveDay").append(newCard);
+    }
+  });
+}
